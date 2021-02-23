d784484e896cebffeb897bd3f9e81720b7023212
Esteban Portillo <portilloesteban1@gmail.com>
2021-02-23 11:42:37 -0400

    last one finished but problems with jasmine

--- spread.js
+++ spread.js
@@ -19,6 +19,7 @@ let product = {
 
 // code here
 
+var productCopy = [...product]
 
 
 
@@ -31,6 +32,7 @@ let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 // code here
 
 
+var colorsCopy = [...colors]
 
 
 ////////// PROBLEM 3 //////////
@@ -44,7 +46,7 @@ function addNums(num1, num2, num3) {
 }
 
 // code here
-
+var result = addNums(...numbers)
 
 ////////// PROBLEM 4 //////////
 
@@ -54,3 +56,4 @@ let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
 // code here
+var combinedArrays = [...arr1, ...arr2] 
